ee4a5b557587a46fa00ab9871be69b76d4535444
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-09-03 19:19:10 -0300

    mini-practica-1

---
+++ mini-practica-1.js
@@ -0,0 +1,29 @@
+// fetch('https://pokeapi.co/api/v2/pokemon/1')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
+
+const axios = require("axios");
+
+async function obtenerPokemon(id) {
+	try {
+		const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+		console.log(`Nombre: ${response.data.name}`);
+
+		console.log(`Altura: ${response.data.height}`);
+
+		console.log(`Peso: ${response.data.weight}`);
+
+		console.log(
+			"Tipos: ",
+			response.data.types.map((t) => t.type.name).join(", ")
+		);
+	} catch (error) {
+		console.error("Error al obtener el Pokémon:", error.message);
+	}
+}
+
+// Ejemplo: obtener el Pokémon con ID 1
+
+obtenerPokemon(1);
